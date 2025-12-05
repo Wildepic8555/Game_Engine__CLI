390b536a769204de808b9f659374abb08b9c86a2
Wildepic8555 <wildepic255555@outlook.com>
2025-12-05 20:39:01 +0100

    test

--- main.c
+++ main.c
@@ -6,14 +6,14 @@
 int main(void) {
     int i = 0;
     while (1) {
-        i++;
-        setSize(15, 5);
+
+        setSize(11, 5);
         setPlayerLocation(9 ,2);
         setPlayerDesign('P');
         setLineDesign('#');
         setObject1(10 ,2 ,'1');
         setObject2(10 ,3 ,'2');
-        scoreBoard(0, 0);
+        scoreBoard(i, i);
         frameUpdate();
 
 
