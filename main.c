8c8dc353c47efdebf613a71bc39f70e1bb2d4f80
Wildepic8555 <wildepic255555@outlook.com>
2025-12-06 16:54:55 +0100

    Made main on top

--- main.c
+++ main.c
@@ -1,15 +1,35 @@
 #include "frameManager.h"
 #include "getKey.h"
 
+//Player position variables and init places
 int player_location_x = 2;
 int player_location_y = 5;
 
 int player2_location_x = 49;
 int player2_location_y = 5;
 
+//Define Map Size
 const int map_x = 50;
 const int map_y = 10;
 
+void player_movement();
+
+int main(void) {
+    //Main Loop
+    while (1) {
+        player_movement();
+        setSize(map_x, map_y);
+        setPlayerLocation(player_location_x ,player_location_y);
+        setPlayerDesign('I');
+        setObject1(player2_location_x ,player2_location_y ,'I');
+        setObject2(25 , 5 , 'o');
+        setLineDesign('#');
+        scoreBoard(9999, 9999);
+        frameUpdate();
+    }
+}
+
+//player 1 and 2 movement with w,s,k,m
 void player_movement() {
     char pressedKey = getKey();
     if ('w' == pressedKey) {
@@ -19,7 +39,7 @@ void player_movement() {
     }
     else if ('s' == pressedKey) {
         if (player_location_y < map_y ) {
-                player_location_y++;
+            player_location_y++;
         }
     }
     else if ('k' == pressedKey) {
@@ -31,21 +51,5 @@ void player_movement() {
         if (player2_location_y < map_y ) {
             player2_location_y++;
         }
-    }
-}
-
-int main(void) {
-    int i = 0;
-    while (1) {
-        i++;
-        player_movement();
-        setSize(map_x, map_y);
-        setPlayerLocation(player_location_x ,player_location_y);
-        setPlayerDesign('I');
-        setObject1(player2_location_x ,player2_location_y ,'I');
-        setObject2(25 , 5 , 'o');
-        setLineDesign('#');
-        scoreBoard(i, i);
-        frameUpdate();
     }
 }
